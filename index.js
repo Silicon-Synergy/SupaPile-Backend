17a064dfb90f016d314bb642da5f2b45fb6b6aa3
Your GitHub Username <Your GitHub Email>
2025-01-04 02:45:49 +0100

    new commit

--- index.js
+++ index.js
@@ -2,30 +2,52 @@ const axios = require("axios");
 const express = require("express");
 const cheerio = require("cheerio");
 const app = express();
-const PORT = 3001;
+const cors = require("cors");
+const PORT = 4501;
+
+app.use(cors());
+
 app.get("/fetchMetaData", async (req, res) => {
   const { url } = req.query;
+  
 
   if (!url) {
     return res.status(400).send({ error: "A URL is required" });
   }
-  try {
-    // i want to make a request to that url
-    const { data } = await axios.get(url);
-    const $ = cheerio.load(data);
-    const title = $("head title").text();
-    const description = $('meta[name="description"]').attr("content");
-    const image = $('meta[property="og:image"]').attr("content");
-    res.json({
-      title,
-      description,
-      image,
+
+  axios
+    .get(url)
+    .then((response) => {
+      const $ = cheerio.load(response.data);
+      console.log(response);
+      const title = $("head title").text();
+      const description = $('meta[name="description"]').attr("content");
+      const image = $('meta[property="og:image"]').attr("content");
+      res.json({
+        title,
+        description,
+        image,
+      });
+    })
+    .catch((error) => {
+      res.status(500).send({ error: "failed to fetch URL" });
     });
-  } catch (err) {
-    res.status(500).send({ error: "failed to fetch URL" });
-  }
+  // try {
+  //   const { data } = await axios.get(url);
+  //   const $ = cheerio.load(data);
+  //   const title = $("head title").text();
+  //   const description = $('meta[name="description"]').attr("content");
+  //   const image = $('meta[property="og:image"]').attr("content");
+  //   res.json({
+  //     title,
+  //     description,
+  //     image,
+  //   });
+  // } catch (err) {
+  //   res.status(500).send({ error: "failed to fetch URL" });
+  // }
 });
 
 app.listen(PORT, () => {
-  console.log("Browser running on port 3001");
+  console.log(`Browser running on port ${PORT} `);
 });
