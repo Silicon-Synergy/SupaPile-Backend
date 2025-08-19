07708ac27de478694086adf18c4873c9d6d6ccb5
superSaiyanSkai <asuleodu@gmail.com>
2025-08-19 11:47:17 +0100

    callback stuff

--- config/passport.js
+++ config/passport.js
@@ -10,7 +10,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.NODE_ENV === "production" 
-        ? "https://supapile-backend.up.railway.app/auth/google/callback"
+        ? process.env.GOOGLE_CALLBACK_URL_PROD
         : process.env.GOOGLE_CALLBACK_URL_DEV
     },
     async (accessToken, refreshToken, profile, cb) => {
